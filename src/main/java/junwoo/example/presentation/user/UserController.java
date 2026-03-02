a6432c7ded60e80dff2e4ad8c2c62754620b8e11
junwoo <jwlee4450@nave.com>
2026-03-02 15:13:22 +0900

    반환 타입 변경

--- src/main/java/junwoo/example/presentation/user/UserController.java
+++ src/main/java/junwoo/example/presentation/user/UserController.java
@@ -1,16 +1,18 @@
 package junwoo.example.presentation.user;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import junwoo.example.shared.ApiResponse;
-import junwoo.example.shared.PageResponse;
 import junwoo.example.application.user.UserService;
 import junwoo.example.domain.user.User;
 import junwoo.example.domain.user.UserRequest;
+import junwoo.example.shared.ApiResponse;
+import junwoo.example.shared.PageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,29 +20,31 @@ import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "UserController", description = "사용자 관련 API")
 public class UserController {
     private final UserService userService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/api/users")
-    public ResponseEntity<ApiResponse<UUID>> createUser(@RequestBody @Valid UserRequest userRequest) {
+    public ApiResponse<UUID> createUser(@RequestBody @Valid UserRequest userRequest) {
         User user = userService.createUser(userRequest);
-        return ApiResponse.create(user.getId());
+        return ApiResponse.of(user.getId());
     }
 
     @GetMapping("/api/users")
-    public ResponseEntity<ApiResponse<PageResponse<UserResponse>>> getUser(
+    public ApiResponse<PageResponse<UserResponse>> getUsers(
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable
     ) {
         Slice<UserResponse> users = userService.getUsers(pageable).map(
                 user -> new UserResponse(user.getId())
         );
 
-        return ApiResponse.ok(PageResponse.of(users));
+        return ApiResponse.of(PageResponse.of(users));
     }
 
     @GetMapping("/api/users/{userId}")
-    public ResponseEntity<ApiResponse<UserResponse>> getUser(@PathVariable UUID userId) {
+    public ApiResponse<UserResponse> getUser(@PathVariable UUID userId) {
         User user = userService.getUser(userId);
-        return ApiResponse.ok(new UserResponse(user.getId()));
+        return ApiResponse.of(new UserResponse(user.getId()));
     }
 }
